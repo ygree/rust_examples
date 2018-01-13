c8d1fbd6c5f7e2f5fc5c6d1aa164d97e4f61e7d3
Yury Gribkov <yury.gribkov@gmail.com>
2018-01-13 17:15:46 -0500

    Fix quick_sort fn name

--- src/bin/quick_sort.rs
+++ src/bin/quick_sort.rs
@@ -9,12 +9,12 @@ fn main() {
 
     let mut arr = vec![94, -42, -17, 98, -71, -95, -42, -96, -6, 20, -23, -95, 2, 65, -40, -61, -90, -45];
 
-    quick_sort_gen(&mut arr);
+    quick_sort(&mut arr);
 
     println!("sorted: {:?}", arr);
 }
 
-fn quick_sort_gen<T: Ord>(arr: &mut [T]) {
+fn quick_sort<T: Ord>(arr: &mut [T]) {
     if arr.len() > 1 {
         let mut a = 0;
         let last = arr.len() - 1;
@@ -32,8 +32,8 @@ fn quick_sort_gen<T: Ord>(arr: &mut [T]) {
             }
         }
         if a > 0 {
-            quick_sort_gen(&mut arr[a..]);
-            quick_sort_gen(&mut arr[..a]);
+            quick_sort(&mut arr[a..]);
+            quick_sort(&mut arr[..a]);
         }
     }
 }
@@ -44,14 +44,14 @@ mod tests {
         fn keeps_length(xs: Vec<i32>) -> bool {
             let arr = &mut xs.clone();
             let len1 = arr.len();
-            quick_sort_gen(arr);
+            quick_sort(arr);
             let len2 = arr.len();
             len1 == len2
         }
 
         fn never_decrease(xs: Vec<i32>) -> bool {
             let arr = &mut xs.clone();
-            quick_sort_gen(arr);
+            quick_sort(arr);
             let mut i = 1;
             while i < arr.len() {
                 if arr[i-1] > arr[i] {
@@ -65,7 +65,7 @@ mod tests {
         fn equals_to_sorted(xs: Vec<i32>) -> bool  {
             let sorted = &mut xs.clone();
             let arr = &mut xs.clone();
-            quick_sort_gen(arr);
+            quick_sort(arr);
 
             sorted.sort();
 
